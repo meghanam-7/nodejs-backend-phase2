39d852e946194ad2ab6c6332bc0ad198e6cb2e4b
Meghana M <mmeghana924@gmail.com>
2026-09-25 10:39:35 +0530

    feat: add assessment retention cleanup

--- src/services/assessmentService.js
+++ src/services/assessmentService.js
@@ -1,9 +1,24 @@
 const crypto = require("crypto");
+const prisma = require("../config/prismaClient");
 
 const generateAssessmentToken = () => {
   return crypto.randomBytes(32).toString("hex");
 };
 
+const purgeExpiredAssessments = async () => {
+  const result = await prisma.jobAssessment.deleteMany({
+    where: {
+      expiresAt: {
+        not: null,
+        lt: new Date(),
+      },
+    },
+  });
+
+  return result.count;
+};
+
 module.exports = {
   generateAssessmentToken,
+  purgeExpiredAssessments,
 };
